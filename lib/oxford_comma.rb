e165a6e2a33e23155ade1b3e0f9d9015fe8a1936
Daniel Kim <daniel.kim52@gmail.com>
2020-02-25 12:27:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-  array.join(' and ') if array.length = 2
+  array.join(' and ') if array.length == 2
 
   array[-1].insert(0, "and ") if array.length > 2
   array.join(", ")
